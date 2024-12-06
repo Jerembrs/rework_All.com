953c7b0469e2a91fbc56f3c6b7ea03c1914753a1
Jerembrs <149231980+Jerembrs@users.noreply.github.com>
2024-12-06 07:50:25 +0100

    init project

---
+++ reworkALL.com/reworkALL.com/reworkALL_comApp.swift
@@ -0,0 +1,17 @@
+//
+//  reworkALL_comApp.swift
+//  reworkALL.com
+//
+//  Created by brs on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct reworkALL_comApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
